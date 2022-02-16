ec8345af0ed8b0b524c3f266a4aefd45f05d5363
ggonep <guillermo.rgo92@gmail.com>
2022-02-16 09:55:18 -0300

    cambios en la ofi

--- models/usuario.js
+++ models/usuario.js
@@ -32,6 +32,7 @@ const UsuarioSchema = Schema({
         default: false
     }
 });
+//prueba
 
 UsuarioSchema.methods.toJSON = function(){
     const {__v, password, ...usuario } = this.toObject();
